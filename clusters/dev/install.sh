221592179c7f655ebcbdcddc3af8ffc3235af360
Mike Hume <mhumesf@gmail.com>
2023-04-07 00:52:34 -1000

    Updating bootstrap instructions

--- clusters/dev/install.sh
+++ clusters/dev/install.sh
@@ -1,9 +1,7 @@
-Bootsrap instructions:
-```
+#!/bin/bash
 kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=v2.6.7
 kubectl create namespace argocd
 export ARGOCD_APP_NAME="argocd-dev"
 helm dependency update ../base > /dev/null && echo "helm dependencies updated"
 helm dependency build ../base > /dev/null && echo "helm dependencies built"
 helm template --release-name ${ARGOCD_APP_NAME} ../base -f ../base/values.yaml > ../base/all.yaml && kustomize build | kubectl apply -f -
-```
